6d5695ae5d29284e47183e65d369a5ad86b6a063
maayan <maayan3002@gmail.com>
2020-06-21 15:03:52 +0300

    init DogGL

---
+++ main.cpp
@@ -0,0 +1,136 @@
+#include "main.h"
+
+// Initializes GLUT, the display mode, and main window; registers callbacks;
+// enters the main event loop.
+int main(int argc, char** argv) {	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(400, 150);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutCreateWindow("dog world");
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutTimerFunc(100, timer, 0);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+}
+
+// Handles the window reshape event
+void reshape(GLint w, GLint h) {
+	glViewport(0, 0, w, h);
+	aspect = float(w / h);
+	updateProjection();
+}
+
+// Handles the display callback to show what we have drawn.
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0, 0, -10);
+	glScalef(1, 1, 1);
+	glRotatef(15, 1.0, 1.0, 1.0);
+	glRotatef(currentAngleOfRotation, 0.0, 1.0, 0.0);
+	/*glLineWidth(4.0);
+	glutWireDodecahedron();*/
+	std::string inputfile = "Retriver.obj";
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+
+	std::string warn;
+	std::string err;
+
+	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, inputfile.c_str());
+
+	if (!warn.empty()) {
+		std::cout << warn << std::endl;
+	}
+
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
+	}
+
+	if (!ret) {
+		exit(1);
+	}
+
+	// Loop over shapes
+	for (size_t s = 0; s < shapes.size(); s++) {		
+		// Loop over faces(polygon)
+		size_t index_offset = 0;
+		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+			int fv = shapes[s].mesh.num_face_vertices[f];
+
+			glBegin(GL_POLYGON);
+			// Loop over vertices in the face.
+			for (size_t v = 0; v < fv; v++) {
+				// access to vertex
+				tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+				tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
+				tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
+				tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
+				tinyobj::real_t nx = attrib.normals[3 * idx.normal_index + 0];
+				tinyobj::real_t ny = attrib.normals[3 * idx.normal_index + 1];
+				tinyobj::real_t nz = attrib.normals[3 * idx.normal_index + 2];
+				tinyobj::real_t tx = attrib.texcoords[2 * idx.texcoord_index + 0];
+				tinyobj::real_t ty = attrib.texcoords[2 * idx.texcoord_index + 1];
+				// Optional: vertex colors
+				tinyobj::real_t red = attrib.colors[3*idx.vertex_index+0];
+				tinyobj::real_t green = attrib.colors[3*idx.vertex_index+1];
+				tinyobj::real_t blue = attrib.colors[3*idx.vertex_index+2];
+
+				// glColor3f(red, green, blue);
+				//std::cout << vx << vy << vz << red << green << blue << std::endl;
+				glColor3f(red, green, blue);
+				glVertex3f(vx, vy, vz);
+			}			
+			index_offset += fv;
+			glEnd();
+			// per-face material
+			// shapes[s].mesh.material_ids[f];
+		}
+		
+	}
+	glRasterPos3f(0.0, 3.0, 0.0);
+	for (char& c : msg) {
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
+	}
+	glFlush();
+	glutSwapBuffers();
+}
+
+// Handles the timer by incrementing the angle of rotation and requesting the
+// window to display again.
+// Since the timer function is only called once, it sets the same function to
+// be called again.
+void timer(int v) {
+	currentAngleOfRotation += 1.0;
+	if (currentAngleOfRotation > 360.0) {
+		currentAngleOfRotation -= 360.0;
+	}
+	glutPostRedisplay();
+	glutTimerFunc(1000 / FPS, timer, v);
+}
+
+// Handles keyboard press
+void keyboard(unsigned char key, int x, int y) {
+	orthoProjection = !orthoProjection;
+	updateProjection();
+}
+
+void updateProjection() {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (orthoProjection) {
+		glOrtho(-10.0, 10.0, -10.0, 10.0, -30.0, 30.0);
+		msg = "ortho";
+		//glColor3f(0.2, 1.0, 0.2);
+	}
+	else {
+		gluPerspective(60.0, aspect, 1, 100.0);
+		msg = "Perspective";
+		//glColor3f(0.0, 0.0, 0.8);
+	}
+	glutPostRedisplay();
+}
---
+++ main.h
@@ -0,0 +1,43 @@
+#include <GL/glut.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <iostream>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+using namespace std;
+
+// Constants
+const int WINDOW_WIDTH = 1000;
+const int WINDOW_HEIGHT = 800;
+const int WINDOW_POS_X = 400;
+const int WINDOW_POS_Y = 150;
+const double PI = 3.14159265358979323846;
+const float WINDOW_RATIO = WINDOW_WIDTH / (float)WINDOW_HEIGHT;
+string msg = "ortho";
+float aspect = WINDOW_RATIO;
+
+// This is the number of frames per second to render.
+static const int FPS = 60;
+
+// This global variable keeps track of the current orientation of the polyhedron.
+static GLfloat currentAngleOfRotation = 0.0;
+
+// Set this to true to change projection.
+static bool orthoProjection = true;
+
+// Function where the scene drawing occures
+void display(void);
+
+// Function for keyboard press
+void keyboard(unsigned char key, int x, int y);
+
+// Function to handle reshape of the screen
+void reshape(GLint w, GLint h);
+
+// Function to handle the timer
+void timer(int v);
+
+// Function to update the projection
+void updateProjection();
